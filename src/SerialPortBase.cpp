59b885866481d7439ff5579ee2c6abdd6a103d11
chenbiwei <chenbiwei@creality.com>
2022-10-27 15:39:41 +0800

    feat:[thirdparty]串口通信库代码
    Change-Id: I6ff8a82724a51ec08cc285d30042c2d7554981b0

---
+++ src/SerialPortBase.cpp
@@ -0,0 +1,46 @@
+﻿#include "CSerialPort/SerialPortBase.h"
+#include "CSerialPort/ithread.hpp"
+
+CSerialPortBase::CSerialPortBase()
+    : p_mutex(NULL)
+    , m_lastError(0)
+    , m_minByteReadNotify(1)
+    , m_operateMode(itas109::AsynchronousOperate)
+{
+    p_mutex = new itas109::IMutex();
+}
+
+CSerialPortBase::CSerialPortBase(const std::string &portName)
+    : p_mutex(NULL)
+    , m_lastError(0)
+    , m_minByteReadNotify(1)
+    , m_operateMode(itas109::AsynchronousOperate)
+{
+    p_mutex = new itas109::IMutex();
+}
+
+CSerialPortBase::~CSerialPortBase()
+{
+    if (p_mutex)
+    {
+        delete p_mutex;
+        p_mutex = NULL;
+    }
+}
+
+void CSerialPortBase::setOperateMode(itas109::OperateMode operateMode)
+{
+    m_operateMode = operateMode;
+}
+
+unsigned int CSerialPortBase::getMinByteReadNotify()
+{
+    return m_minByteReadNotify;
+}
+
+int CSerialPortBase::getLastError() const
+{
+    return m_lastError;
+}
+
+void CSerialPortBase::clearError() {}
